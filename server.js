bf36039552c2f3382aa524485fc85b18a3a9efe6
tquang2410 <justicepencil@gmail.com>
2025-07-24 10:53:40 +0700

    Edit README.md

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 
 // Middleware
 app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }));
 app.use(express.json());
 
 // Routes
